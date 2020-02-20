b6fdb5279811869d86126ff3a5f3bf53e4bf817f
Austin McElroy <mcelroy.austin@gmail.com>
2020-02-19 21:14:08 -0600

    Renamed EEGLib.py to EEG.py to fix error in module names Added some example code that fetches data from the .mat stuct, like the intended letter and the ringBuffer, whatever the ringBuffer is
    Added code to get the Raw EEG over all channels
    
    Added code to split the eeg data from the trigger

--- EEGLib/EEG.py
+++ EEGLib/EEG.py
@@ -3,6 +3,7 @@ import scipy
 from scipy import io
 from mne import Epochs
 from mne.channels import Layout
+import numpy as np
 import os
 from pathlib import Path
 
@@ -11,13 +12,15 @@ from EEGLib.EE385VMatFile import EE385VMatFile
 
 class EEG:
     def __init__(self):
-        self.__gdf = []
-        self.__mat = []
+        self.__gdf = mne.io.Raw
+        self.__mat = np.ndarray
+        self.__eeg = mne.io.Raw
+        self.__trigger = mne.io.Raw
 
     def open(self, filepath):
         '''
         @param filepath: Path to GDF or .MAT file
-        @:return Tuple (gdf, EE385VMatFile)
+        @:return Tuple (EEG data scaled to V, trigger, EE385VMatFile)
         '''
 
         path = Path(filepath)
@@ -36,8 +39,17 @@ class EEG:
         gdf = mne.io.read_raw_gdf(gdf_path, preload=True)
         mat = EE385VMatFile(mat_path)
         self.__gdf = gdf
+        # Scale to microvolts, see: https://github.com/mne-tools/mne-python/issues/5539
+        self.__eeg, self.__trigger = self.splitTrigger(trigger='trigger')
+        self.__eeg = self.__eeg.apply_function(lambda x: x / 1e6)
         self.__mat = mat
-        return (gdf, mat)
+        return (self.__eeg, self.__trigger, mat)
+
+    def getChannels(self):
+        return self.__gdf.ch_names
+
+    def printChannels(self):
+        print('Availible Channels: ' + str(self.getChannels()))
 
     def getRawEEG(self, channels=['']):
         '''
@@ -47,16 +59,46 @@ class EEG:
         :return: Tuple of ([EEG Channel, Data], Time)
         '''
         if channels == ['']:
-            return gdf.get_data(return_times=True)
+            return self.__gdf.get_data(return_times=True)
         else:
-            return gdf.get_data(picks=channels, return_times=True)
+            return self.__gdf.get_data(picks=channels, return_times=True)
+
+    def splitTrigger(self, trigger='trigger'):
+        '''
+        Splits the trigger from the EEG signal
+
+        :param trigger: Name of the trigger. i.e. trigger if the channel is trigger:1
+        :return: Tuple (Data without Trigger, Trigger)
+        '''
+        for channels in self.getChannels():
+            if trigger in channels:
+                trigger = self.__gdf.get_data(picks=channels)
+                data_no_trig = self.__gdf.drop_channels(channels)
+                return (data_no_trig, trigger)
+
+
+
+        return self.__gdf
+
+    def getAlpha(self):
+        return self.__gdf.filter(l_freq=8, h_freq=12)
+
+    def getTheta(self):
+        return self.__gdf.filter(l_freq=1, h_freq=8)
+
+    def getBeta(self):
+        return self.__gdf.filter(l_freq=12, h_freq=30)
 
 
 if __name__ == "__main__":
     e = EEG()
-    (gdf, mat) = e.open(
+    (eeg, trig, mat) = e.open(
         '/home/amcelroy/Code/EE385V/BCI Course 2020/ErrPSpeller/Subject1/Offline/ad4_raser_offline_offline_171110_172431.gdf')
 
+    e.printChannels()
+    rawEEG = e.getRawEEG()
+    eeg.plot(highpass=1, lowpass=40, filtorder=6, decim=1, n_channels=8)
+    #gdf.printChannels()
     channels = e.getRawEEG()
     mat.targetLetter()
     mat.ringBuffer()
