a7da7e7ca009480d5bb194251b4491a30f059716
kliment-slice <61919865+kliment-slice@users.noreply.github.com>
2020-04-21 13:28:22 -0500

    Function to load online dataset error and no_error

---
+++ EEGLib/DatasetLoader.py
@@ -0,0 +1,51 @@
+import os
+
+
+class DatasetLoader:
+    def __init__(self, root_folder):
+        self.__root = root_folder
+        self.__subject_dict = {}
+        self.load_subjects()
+
+    def load_subjects(self):
+        data = {}
+        folder = self.__root
+        for subject in os.listdir(folder):
+            if os.path.isdir(os.path.join(folder, subject)):
+                data[subject] = {}
+                for trial in os.listdir(os.path.join(folder, subject)):
+                    if os.path.isdir(os.path.join(folder, subject, trial)):
+                        data[subject][trial] = []
+                        for run in os.listdir(os.path.join(folder, subject, trial)):
+                            if run.endswith('.gdf'):
+                                data[subject][trial].append(run)
+        self.__subject_dict = data
+        return data
+
+#    def getOffline(self):
+#        data = self.__subject_dict
+#        offline = {}
+#        for subject in data.keys():
+#            offline[subject] = []
+#            for trials in data[subject].keys():
+#                if trials == 'Offline':
+#                    offline[subject] = data[subject][trials]
+#        return offline
+    def getTrials(self):
+        data = self.__subject_dict
+        offline = {}
+        S2 = {}
+        S3 = {}
+        for subject in data.keys():
+            offline[subject] = []
+            for trials in data[subject].keys():
+                if trials == 'Offline':
+                    offline[subject] = data[subject][trials]
+                elif trials == 'S2':
+                    S2[subject] = data[subject][trials]
+                else:
+                    S3[subject] = data[subject][trials]
+        return offline, S2, S3
+    
+    def getAll(self):
+        return self.__subject_dict
